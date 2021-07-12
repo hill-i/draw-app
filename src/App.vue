7c645772a738371135d63383c3b3c77dbee01c41
ひるあい <hill_i@hiruainoMacBook-Pro.local>
2021-07-12 18:57:40 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex-row">
+    <side-bar class=""></side-bar>
+    <div class="flex-shrink-0 w-9/12">コンテンツ</div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import SideBar from './components/SideBar.vue'
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      SideBar,
+    },
+  })
+</script>
---
+++ src/components/SideBarItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4 text-center cursor-pointer hover:bg-gray-100" @click="onClick">{{ title }}</div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({
+    name: 'SideBarItem',
+    props: {
+      title: String,
+      onClick: Function,
+    },
+  })
+</script>
