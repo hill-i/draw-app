8c8ee7b2a32b1aa1fb31f2973f47d1c6388f8a60
ひるあい <hill.i.stream@gmail.com>
2021-07-24 16:28:51 +0900

    socket通信疎通確認

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <app-bar></app-bar>
     <div class="w-3/4">
       <span>コンテンツ</span>
+      <socket-test></socket-test>
     </div>
   </div>
 </template>
@@ -10,11 +11,13 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   import AppBar from './components/AppBar.vue'
+  import SocketTest from "@/components/SocketTest.vue";
 
   export default defineComponent({
     name: 'App',
     components: {
       AppBar,
+      SocketTest
     },
   })
 </script>
---
+++ src/components/SocketTest.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+import { connectServer } from "@/utils/socket";
+
+export default defineComponent({
+    setup() {
+        onMounted(() => {
+            connectServer()
+        })
+    },
+})
+</script>
